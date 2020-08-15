37fa434687896bf50c9401eaca2bec3feaac1643
twarbelow <taija.warbelow@google.com>
2020-08-15 16:53:16 -0700

    remove unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   get '/registration', to: 'users#new', as: :registration
   resources :users, only: [:create, :update]
-  patch '/user/:id', to: 'users#update'
+  # patch '/user/:id', to: 'users#update'
 
   get '/profile/edit_password', to: 'users#edit_password'
   get '/profile/orders', to: 'user/orders#index'
@@ -28,8 +28,6 @@ Rails.application.routes.draw do
   delete '/profile/orders/:id', to: 'user/orders#cancel'
   post '/orders', to: 'user/orders#create'
   resources :users, only: [:show, :edit], as: :profile, path: :profile
-  # get '/profile', to: 'users#show'
-  # get '/profile/edit', to: 'users#edit'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
